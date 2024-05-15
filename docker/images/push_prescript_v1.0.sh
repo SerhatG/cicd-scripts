bc0798fa8f96926f85c8461d845308c82200e1c4
Serhat Gülçiçek <serhat+github@equil.nl>
2024-05-15 14:23:08 +0200

    WIP

--- docker/images/push_prescript_v1.0.sh
+++ docker/images/push_prescript_v1.0.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-# This prescript is superseded by images_v*.sh, but is still there for backwards compatibility
-
 # Exit on error
 set -e
-
-# Tools needed for this script to function
-export CICD_SCRIPTS_TOOL_YQ=true
 
 # Record directory of script for convenience
 SCRIPT_PATH=$(readlink -f "${0}")
 SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
 
-# Prepare stuff
-source "${SCRIPT_DIR}"/../prepare/make_it_so.envsh
+# include functions
+source "${SCRIPT_DIR}"/functions_v1.0.sh
 
 # We should only proceed at this time if we find an ECR URL
 if [[ "${AERIUS_REGISTRY_URL}" != *.ecr.*.amazonaws.com/* ]]; then
